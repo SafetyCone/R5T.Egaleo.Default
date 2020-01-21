a939e49464876eff4125fb7c4ec5ace32007d3f6
MinexAutomation <minex.automation@gmail.com>
2020-01-21 12:31:08 -0800

    Added default OrganizationDirectoryNameConvention implementation of IOrganizationDirectoryNameConvention service.

---
+++ source/R5T.Egaleo.Default/Code/Services/Implementations/OrganizationDirectoryNameConvention.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Egaleo.Default
+{
+    /// <summary>
+    /// The default organization directory name is just the organization name.
+    /// </summary>
+    public class OrganizationDirectoryNameConvention : IOrganizationDirectoryNameConvention
+    {
+        public string GetOrganizationDirectoryName(string organizationName)
+        {
+            // The organization directory name is just the organization name.
+            return organizationName;
+        }
+    }
+}
