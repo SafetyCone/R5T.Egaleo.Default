723141276ed70a4f14df617299475438e6ef7d54
MinexAutomation <minex.automation@gmail.com>
2020-01-30 15:14:14 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Egaleo.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Egaleo.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="OrganizationDirectoryNameConvention"/> implementation of <see cref="IOrganizationDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultOrganizationDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IOrganizationDirectoryNameConvention, OrganizationDirectoryNameConvention>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="OrganizationDirectoryNameConvention"/> implementation of <see cref="IOrganizationDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IOrganizationDirectoryNameConvention> AddDefaultOrganizationDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IOrganizationDirectoryNameConvention>(() => services.AddDefaultOrganizationDirectoryNameConvention());
+            return serviceAction;
+        }
+    }
+}
