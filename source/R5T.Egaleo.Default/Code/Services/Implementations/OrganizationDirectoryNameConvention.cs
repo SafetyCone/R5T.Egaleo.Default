d43348e304648fe15f5cbf117353b62cd411025f
davidcoats <david@getsafetycone.com>
2022-04-27 15:30:49 -0700

    Interim changes.

--- source/R5T.Egaleo.Default/Code/Services/Implementations/OrganizationDirectoryNameConvention.cs
+++ source/R5T.Egaleo.Default/Code/Services/Implementations/OrganizationDirectoryNameConvention.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Egaleo.Default
-{
+{[ServiceImplementationMarker]
     /// <summary>
     /// The default organization directory name is just the organization name.
     /// </summary>
-    public class OrganizationDirectoryNameConvention : IOrganizationDirectoryNameConvention
+    public class OrganizationDirectoryNameConvention : IOrganizationDirectoryNameConvention,IServiceImplementation
     {
         public string GetOrganizationDirectoryName(string organizationName)
         {
